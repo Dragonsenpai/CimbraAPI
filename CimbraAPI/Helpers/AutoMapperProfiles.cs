fe2d90c82ae45c6bef3f84a4207a537d6d99abbc
MarcoRagnar <marco.huerta.sc@pm.me>
2020-08-18 16:13:29 -0500

    CRUD API

---
+++ CimbraAPI/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CimbraAPI.DTOs;
+using CimbraAPI.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CimbraAPI.Helpers
+{
+    public class AutoMapperProfiles: Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Genero, GeneroDTO>().ReverseMap();
+            CreateMap<GeneroCreacionDTO, Genero>().ReverseMap();
+
+        }
+    }
+}
